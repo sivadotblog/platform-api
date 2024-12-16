03ba224827916b006c01bd9df7deb0975af4f184
Siva Rajadurai <hello@siva.blog>
2024-12-16 07:51:17 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(() => {
   return {
     plugins: [react()],
-    base: "./",
+    base: "https://siva-platform-api.azurewebsites.net/",
     server: {
       port: 3000,
     },
